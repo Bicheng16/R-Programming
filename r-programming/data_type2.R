053d7ee2a8a49063be813e4b7a9b3df31ca092dd
Bicheng16 <yan.bicheng@gmail.com>
2018-06-07 06:22:46 -0500

    data frame, names etc

--- r-programming/data_type2.R
+++ r-programming/data_type2.R
@@ -1,4 +1,4 @@
-# Data missing values
+## Data missing values
 
 # NaN/NA: undefined mathematical operations, NaN is a subset of NA
 # NA can be int, char, etc; NaN only int
@@ -17,3 +17,25 @@ is.nan(x) # works: FALSE FALSE  TRUE FALSE FALSE
 x <- c(1,2,NaN,NA, 10,3)
 is.na(x) # works: FALSE FALSE  TRUE  TRUE FALSE FALSE
 is.nan(x) # fails: FALSE FALSE  TRUE FALSE FALSE FALSE
+
+
+## Data frames: 2D list; can store different calss of objects in each column
+# specical attributes: row.names
+# created by calling read.table() or read.csv()
+# convert to matrix by calling data.matrix()
+x <- read.csv("../ds-student/r-programming/example.csv")
+nrow(x)
+ncol(x)
+data.matrix(x)
+
+x <- data.frame(ID = c("James", "Dylan", "Rod"), Balance = c(1000, 250000, 300))
+nrow(x)
+ncol(x)
+data.matrix(x)
+
+## Names
+# 
+x <- 1:3
+names(x)
+names(x) <- c("foo", "bar", "norf")
+x
